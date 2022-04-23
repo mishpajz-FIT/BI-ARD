e418e77efd7d548bffb4400e8f560192003fe9c0
Michal Dobeš <dobesmic@fit.cvut.cz>
2022-04-23 21:48:15 +0200

    Create test for tempeature and humidity sensor

--- src/teplomer/teplomer.ino
+++ src/teplomer/teplomer.ino
@@ -1,9 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
+#include <DHT_U.h>
 
+#define tempPIN 3
+#define dhtType DHT22
+
+DHT tempSensor(tempPIN, dhtType);
+
+void setup() {
+  Serial.begin(9600);
+  tempSensor.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float t = tempSensor.readTemperature();
+  float h = tempSensor.readHumidity();
 
+  if (!isnan(t) && !isnan(h)) {
+    Serial.print("tempeature: ");
+    Serial.print(t);
+    Serial.print("; ");
+    Serial.print("humidity: ");
+    Serial.println(h);
+  } else {
+    Serial.println("tmp: err");
+  }
+
+  delay(3000);
 }
